216aa8b76a2beb3a3dcdfb56e62b95c195017a3f
Ninos Pito <Ninospito@hotmail.com>
2026-02-26 13:41:06 +0100

    fixed js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-const accordion = document.getElementsByClassName('content-container')
+const accordion = document.getElementsByClassName('content-container');
 
-for(i = 0; i <accordion.length; i++){
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active');
-    });
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+
+    const isActive = this.classList.contains('active');
+
+    for (let j = 0; j < accordion.length; j++) {
+      accordion[j].classList.remove('active');
+    }
+
+    if (!isActive) {
+      this.classList.add('active');
+    }
+
+  });
 }
